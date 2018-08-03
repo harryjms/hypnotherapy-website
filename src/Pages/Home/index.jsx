90dafcd16ec30a13b29b11bd5f802fd8d8f61f7b
Harry Jarman <me@harryjms.uk>
2018-08-03 20:35:15 +0100

    changes

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,9 +1,49 @@
 import React from 'react';
+import injectSheet from 'react-jss';
 import Content from '../../Components/Layout/Content.jsx';
 import { Carousel, Slide } from '../../Components/Carousel/index.jsx';
-import stress from '../../../assets/images/heros/stress.png';
+import calm from '../../../assets/images/heros/calm.jpg';
+import weightloss from '../../../assets/images/heros/weightloss.jpg';
+import { combineClasses } from '../../Utils/jss';
 
-console.log(Carousel);
+const styles = theme => ({
+  topLine: {
+    color: theme.colours.darkPink,
+  },
+  calmSlide: {
+    display: 'flex',
+    alignItems: 'center',
+    backgroundImage: `url(${calm})`,
+    backgroundPosition: 'center',
+    backgroundSize: 'cover',
+    height: 300,
+    justifyContent: 'center',
+  },
+  weightLossSlide: {
+    display: 'flex',
+    alignItems: 'center',
+    backgroundImage: `url(${weightloss})`,
+    backgroundPosition: 'center',
+    backgroundSize: 'cover',
+    height: 300,
+    justifyContent: 'center',
+  },
+  slideContent: {
+    backgroundColor: 'rgba(255,255,255,0.5)',
+    borderRadius: 10,
+    padding: 10,
+    textAlign: 'center',
+  },
+  slideText: {
+    fontSize: '30pt',
+    fontWeight: 'bold',
+    letterSpacing: 0.5,
+    lineHeight: 0.9,
+    margin: 0,
+    textAlign: 'center',
+    textTransform: 'uppercase',
+  },
+});
 
 class Home extends React.Component {
   constructor(props) {
@@ -11,16 +51,49 @@ class Home extends React.Component {
   }
 
   render() {
+    const { classes } = this.props;
     return (
-      <Content>
-        <Carousel>
-          <Slide>
-            <img src={stress} />
-          </Slide>
-        </Carousel>
-      </Content>
+      <div>
+        <div>
+          <div className={classes.calmSlide}>
+            <Content style={{ paddingLeft: 20, paddingRight: 20 }}>
+              <div className={classes.slideContent}>
+                <p
+                  className={combineClasses(classes.slideText, classes.topLine)}
+                >
+                  Break Free
+                </p>
+                <p className={classes.slideText}>from Stress and Anxiety</p>
+                <p>
+                  Hypnotherapy, particularly Cognitive Behavioural Hypnotherapy,
+                  is one of the most effective treatments for anxiety, stress,
+                  social anxiety and panic attacks.
+                </p>
+              </div>
+            </Content>
+          </div>
+        </div>
+        <div>
+          <div className={classes.weightLossSlide}>
+            <Content style={{ paddingLeft: 20, paddingRight: 20 }}>
+              <div className={classes.slideContent}>
+                <p
+                  className={combineClasses(classes.slideText, classes.topLine)}
+                >
+                  Want to
+                </p>
+                <p className={classes.slideText}>Lose Weight?</p>
+                <p>
+                  Try our revolutionary weight loss programme, Hypnodieting the
+                  non-diet diet programme.
+                </p>
+              </div>
+            </Content>
+          </div>
+        </div>
+      </div>
     );
   }
 }
 
-export default Home;
+export default injectSheet(styles)(Home);
